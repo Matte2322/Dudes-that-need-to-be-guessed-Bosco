7022287fb141426b0e69e965e2682407563651ca
Matte2322 <matthewchen1688@gmail.com>
2022-10-27 09:11:56 -0700

    fixed some bugs about repeating output values

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random, time 
 from openFiles import *
 from sequenceSearch import *
+from random import sample
 
 answer = 'n'
 guesses = 0
@@ -38,8 +39,8 @@ def students(inputStuff):
                 time.sleep(0.5)
                 print('.')
             guesses+=1
-            randomTech = random.choice(technologyDepartments)
-            answer = input('Is your technology ' + randomTech + "?: ")
+            randomTech = random.sample(technologyDepartments, k=1)
+            answer = input('Is your technology ' + str(randomTech) + "?: ")
     print('Wow, took me ' + str(guesses) + " guess(es) to find what tech you're in ")
     guessingStudentName()
 
